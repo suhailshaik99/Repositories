95f3caf25df0b31244371de1c3c6bf4cf7307c61
Shaik Suhail <suhailshaik975@gmail.com>
2024-12-18 11:56:41 +0530

    no changes in this commit

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,12 +1,12 @@
+import { useState } from "react";
 import { themeContext } from "./contexts/ThemeContext";
 import ParentComponent from "./components/ParentComponent";
-import { useState } from "react";
 
 function App() {
   const [mode, setMode] = useState("light");
   return (
     <div className="App">
-      <themeContext.Provider value={{mode, setMode}}>
+      <themeContext.Provider value={{ mode, setMode }}>
         <ParentComponent />
       </themeContext.Provider>
     </div>
