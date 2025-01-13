0a3c99f3eca397bd490f8e75abf700583070e063
Shaik Suhail <suhailshaik975@gmail.com>
2025-01-13 07:29:31 +0530

    All(necessary) Life Cycle methods are defined

--- classbasedcomponents/src/components/navbar.js
+++ classbasedcomponents/src/components/navbar.js
@@ -6,6 +6,7 @@ class Navbar extends Component {
     this.state = {
       count: 0,
       theme: "Light",
+      showChild: true,
     };
   }
 
@@ -16,20 +17,37 @@ class Navbar extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.count !== prevState.count) {
-      console.log(prevState.count);
       console.log("Component Did Update Executed!!!");
     }
   }
 
-  
+  shouldComponentUpdate(prevProps, prevState) {
+    const updateStatus =
+      prevState.showChild !== this.state.showChild ||
+      prevState.count !== this.state.count;
+    if (updateStatus) console.log("Should Component Update Executed!!");
+    return updateStatus;
+  }
 
   render() {
-    const { count, theme } = this.state;
+    const { count, theme, showChild } = this.state;
     return (
       <>
         <h3 style={{ textAlign: "center" }}>This is Navbar component.</h3>
         <h1 style={{ textAlign: "center" }}>Count is: {count}</h1>
         <p style={{ textAlign: "center" }}>Theme Status: {theme}</p>
+        {showChild && <Child />}
+        <p style={{ textAlign: "center" }}>
+          <button
+            onClick={() =>
+              this.setState((prevState) => ({
+                showChild: !prevState.showChild,
+              }))
+            }
+          >
+            {this.state.showChild ? "Turn me off" : "Turn me on"}
+          </button>
+        </p>
         <button
           onClick={() =>
             this.setState({ count: this.state.count + 1, theme: "Dark" })
@@ -45,6 +63,27 @@ class Navbar extends Component {
           Decrement
         </button>
       </>
+    );
+  }
+}
+
+class Child extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favouriteFood: "Pizza",
+    };
+  }
+
+  componentWillUnmount() {
+    console.log("Component Will Unmount executed!!");
+  }
+
+  render() {
+    return (
+      <p style={{ textAlign: "center" }}>
+        My favourite food is: {this.state.favouriteFood}
+      </p>
     );
   }
 }
